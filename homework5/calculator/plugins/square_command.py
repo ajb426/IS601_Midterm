617ed40c25ad6f5375c4d2c6f834301028b99734
Anthony Barone <ajb42686@yahoo.com>
2024-06-16 21:34:59 -0400

    added square command to history

--- homework5/calculator/plugins/square_command.py
+++ homework5/calculator/plugins/square_command.py
@@ -1,5 +1,8 @@
 from calculator.command import Command
+from calculator.calculator import Calculator
 
 class SquareCommand(Command):
-    def execute(self, x):
-        return x * x
+    def execute(self, operand):
+        result = operand * operand
+        Calculator._store_calculation("square", operand, operand, result)
+        return result
